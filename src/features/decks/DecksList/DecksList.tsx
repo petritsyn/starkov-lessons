dca133cdc192fbc3be95fbb2bd61ef36acc6a5fe
petritsyn <andrey2343@yandex.ru>
2024-05-30 01:45:36 +0300

    get request

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -1,5 +1,13 @@
+import { useEffect } from 'react'
 import s from './DecksList.module.css'
+import { decksAPI, instance } from '../decks-api'
 
 export const DecksList = () => {
+
+  useEffect(() => {
+    decksAPI.fetchDecks()
+  }, [])
+
   return <ul className={s.list}></ul>
 }
+
--- src/features/decks/decks-api.ts
+++ src/features/decks/decks-api.ts
@@ -6,3 +6,35 @@ export const instance = axios.create({
     'x-auth-skip': true,
   },
 })
+
+
+export const decksAPI = {
+  fetchDecks() {
+    return instance.get<ResponseType>('/v2/decks').then(res => console.log(res.data))
+  }
+}
+
+type ResponseType = {
+  "items": [
+    {
+      "author": {
+        "id": "string",
+        "name": "string"
+      },
+      "id": "string",
+      "userId": "string",
+      "name": "string",
+      "isPrivate": true,
+      "cover": "string",
+      "created": string,
+      "updated": string,
+      "cardsCount": number
+    }
+  ],
+  "pagination": {
+    "currentPage": number,
+    "itemsPerPage": number,
+    "totalPages": number,
+    "totalItems": number
+  }
+}
